fab2634c9ca05c5480ae3663cefcbcb55db5ac4a
Lovethrech <rachealdolapo45@gmail.com>
2025-07-20 02:18:09 +0100

    ...working on the project.

--- src/components/CheckOutMainCtn.vue
+++ src/components/CheckOutMainCtn.vue
@@ -1,10 +1,11 @@
 <script setup>
+import AuthMainBgCtn from './AuthMainBgCtn.vue';
 </script>
 
 <template>
     <div class="check-out-main-ctn-main-ctn">
         <div class="check-out-main-ctn-mini-ctn">
-
+            <AuthMainBgCtn></AuthMainBgCtn>
         </div>
     </div>
 </template>
