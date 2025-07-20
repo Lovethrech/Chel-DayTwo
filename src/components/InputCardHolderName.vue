3a908d39688eb8b5017c83114549dee9a6119c62
Lovethrech <rachealdolapo45@gmail.com>
2025-07-20 16:48:08 +0100

    ...working on the project.

---
+++ src/components/InputCardHolderName.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="auth-main-content-form-input-and-label-ctn">
+            <input 
+                v-model="cardHolderName" 
+                :type="creditCardDetails[0].nameType" 
+                @click="iClick" 
+                :id="creditCardDetails[0].nameFor" 
+                :placeholder="creditCardDetails[0].namePlaceholder"
+                class="auth-main-content-form-input" 
+            />
+            <label 
+                :for="creditCardDetails[0].nameFor" 
+                :style="{ top: namePositionPlacement }" 
+                class="auth-main-content-form-label"
+            >
+                {{ creditCardDetails[0].name }}
+            </label>
+        </div>
+</template>
