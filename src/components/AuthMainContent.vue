ec9d8b6c6fc6b5aa1a2b613439c44ec5b56c24bc
Lovethrech <rachealdolapo45@gmail.com>
2025-07-20 14:27:37 +0100

    ...working on the project.

--- src/components/AuthMainContent.vue
+++ src/components/AuthMainContent.vue
@@ -5,7 +5,19 @@ import AuthMainContentFormCtn from './AuthMainContentFormCtn.vue';
 <template>
     <div class="auth-main-content-main-ctn">
         <div class="auth-main-content-mini-ctn">
+            <h1 class="auth-main-content-mini-ctn-title">
+                Payment Method
+            </h1>
+            <p class="auth-main-content-mini-ctn-desc">
+
+            </p>
             <AuthMainContentFormCtn></AuthMainContentFormCtn>
         </div>
     </div>
 </template>
+
+<style scoped>
+.auth-main-content-main-ctn{
+    color:#fff;
+}
+</style>
