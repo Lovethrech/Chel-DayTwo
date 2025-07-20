acaf4e464d4cdceb8851096ba29e6e8554e1baf2
Lovethrech <rachealdolapo45@gmail.com>
2025-07-20 02:55:34 +0100

    ...working on the project.

--- src/components/AuthMainContent.vue
+++ src/components/AuthMainContent.vue
@@ -1,7 +1,11 @@
+<script setup>
+import AuthMainContentFormCtn from './AuthMainContentFormCtn.vue';
+</script>
+
 <template>
     <div class="auth-main-content-main-ctn">
         <div class="auth-main-content-mini-ctn">
-            
+            <AuthMainContentFormCtn></AuthMainContentFormCtn>
         </div>
     </div>
 </template>
