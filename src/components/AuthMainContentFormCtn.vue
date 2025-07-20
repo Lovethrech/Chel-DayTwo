5e3e13a085a9f587fed84c93ce82b73e60acf3c7
Lovethrech <rachealdolapo45@gmail.com>
2025-07-20 02:54:51 +0100

    ...working on the project.

---
+++ src/components/AuthMainContentFormCtn.vue
@@ -0,0 +1,7 @@
+<template>
+    <form action="" class="auth-main-content-form-main-ctn">
+        <div class="auth-main-content-form-input-and-label-ctn">
+            
+        </div>
+    </form>
+</template>
