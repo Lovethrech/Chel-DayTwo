ab24945338cefb68b521930c65accd5b1752cd81
Lovethrech <rachealdolapo45@gmail.com>
2025-07-20 02:58:49 +0100

    ...working on the project.

--- src/components/AuthMainContentCtn.vue
+++ src/components/AuthMainContentCtn.vue
@@ -6,6 +6,7 @@ import AuthMainContent from './AuthMainContent.vue';
     <div class="auth-main-content-ctn-main-ctn">
         <div class="auth-main-content-ctn-mini-ctn">
             <!-- img-ctn  -->
+            <!-- search for types top 10 type of credit sards both internationally and in nigeria only  -->
             <AuthMainContent></AuthMainContent>
         </div>
     </div>
