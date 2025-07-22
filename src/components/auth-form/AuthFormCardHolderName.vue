4d741f54cbead906112a08581ee08cf27635eebd
Lovethrech <rachealdolapo45@gmail.com>
2025-07-22 17:01:54 +0100

    ...working on the project.

--- src/components/auth-form/AuthFormCardHolderName.vue
+++ src/components/auth-form/AuthFormCardHolderName.vue
@@ -2,6 +2,14 @@
 import { defineProps } from 'vue';
 
 const {cardHolderName, nameType, nameFor, namePlaceholder, namePositionPlacement, name, iClick}=defineProps(['cardHoldeName', 'nameType', 'nameFor', 'namePlaceholder', 'namePositionPlacement', 'name', 'iClick']);
+const iClickCardHolderName=()=>{
+    if ((cardHolderName.value==="")){
+        namePositionPlacement.value="-16px";
+    }
+    else{
+        namePositionPlacement.value="50%";
+    }
+}
 </script>
 
 <template>
@@ -9,7 +17,7 @@ const {cardHolderName, nameType, nameFor, namePlaceholder, namePositionPlacement
         <input 
             v-model="cardHolderName" 
             :type="creditCardDetails[0].nameType" 
-            @click="iClick" 
+            @click="iClickCardHolderName" 
             :id="creditCardDetails[0].nameFor" 
             :placeholder="creditCardDetails[0].namePlaceholder"
             class="auth-main-content-form-input" 
