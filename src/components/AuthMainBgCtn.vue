61b86b117fddcd5f7a61445b0b6ebca65f0bd396
Lovethrech <rachealdolapo45@gmail.com>
2025-07-22 19:33:02 +0100

    ...working on the project.

--- src/components/AuthMainBgCtn.vue
+++ src/components/AuthMainBgCtn.vue
@@ -22,6 +22,11 @@ import CheckOutMainCtn from './CheckOutMainCtn.vue';
         width:90%;
     }
 }
+@media screen and (max-height: 690px) {
+    .landing-page-nav-mini-ctn{
+        height: 120vh;
+    }
+}
 </style>
 <template>
     <div class="landing-page-nav-mini-ctn">
